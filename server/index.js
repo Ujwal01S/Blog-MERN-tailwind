d9ca5c3783486a70142593cc94644dd264f3dae3
Ujwal01S <98069995+Ujwal01S@users.noreply.github.com>
2024-09-02 17:32:36 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoute from './routes/user.routes.js'
+import authRoute from './routes/auth.routes.js'
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+
+
 
 
 dotenv.config();
@@ -16,9 +17,13 @@ mongoose.connect(process.env.MONGO)
 .catch(err => console.log(err));
 
 
-const __dirname = path.resolve();
 
-const app = express()
+
+const app = express();
+
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -26,16 +31,13 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");   
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+
 
 
 
